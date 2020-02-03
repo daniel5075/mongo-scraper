1caa5cbc0175ffa2af747fbb830333101d4449bc
Daniel Groveman <daniel5075@gmail.com>
2020-02-02 22:40:45 -0600

    comment out api & html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
-var db = require("../models");
+// var db = require("../models");
 
-module.exports = function (app) {
-  // Get all examples
-  app.get("/api/article", function (req, res) {
-    db.Article.findAll({}).then(function (dbArticle) {
-      res.json(dbArticle);
-    });
-  });
+// module.exports = function (app) {
+//   // Get all examples
+//   app.get("/api/article", function (req, res) {
+//     db.Article.findAll({}).then(function (dbArticle) {
+//       res.json(dbArticle);
+//     });
+//   });
 
-  // Create a new example
-  app.post("/api/article", function (req, res) {
-    db.Article.create(req.body).then(function (dbArticle) {
-      res.json(dbArticle);
-    });
-  });
+//   // Create a new example
+//   app.post("/api/article", function (req, res) {
+//     db.Article.create(req.body).then(function (dbArticle) {
+//       res.json(dbArticle);
+//     });
+//   });
 
-  // Delete an example by id
-  app.delete("/api/article/:id", function (req, res) {
-    db.Article.destroy({ where: { id: req.params.id } }).then(function (dbArticle) {
-      res.json(dbArticle);
-    });
-  });
-};
+//   // Delete an example by id
+//   app.delete("/api/article/:id", function (req, res) {
+//     db.Article.destroy({ where: { id: req.params.id } }).then(function (dbArticle) {
+//       res.json(dbArticle);
+//     });
+//   });
+// };
